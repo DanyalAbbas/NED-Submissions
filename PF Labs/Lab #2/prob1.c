aba82ed9e5a76999e279604dbfae725aa6f65263
Danyal Abbas <abasdaniel61@gmail.com>
2024-09-24 19:40:28 +0500

    [Added] All the lab submissions from Lab 2

---
+++ PF Labs/Lab #2/prob1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    printf("wanna play beep five times ji? >:3");
+    printf("\a\n");
+    getch();
+    printf("\a\n");
+    getch();
+    printf("\a\n");
+    getch();
+    printf("\a\n");
+    getch();
+    printf("\a\n");
+    getch();
+}
