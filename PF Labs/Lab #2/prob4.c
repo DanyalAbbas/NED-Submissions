fdba0518db0082e65e98c0b4594da903dd086649
Danyal Abbas <abasdaniel61@gmail.com>
2024-09-28 18:13:04 +0500

    [Completed] all the tasks from lab 2

---
+++ PF Labs/Lab #2/prob4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("*********\n*\t*\n*\t*\n*\t*\n*\t*\n*\t*\n*\t*\n*\t*\n*********");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("  *****  \n *     * \n*       *\n*       *\n*       *\n *     * \n  *****  \n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("  *\n ***\n*****\n  *\n  *\n  *\n  *\n  *\n  *\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("    *    \n   * *   \n  *   *  \n *     * \n*       *\n *     * \n  *   *  \n   * *   \n    *    \n");
+}
