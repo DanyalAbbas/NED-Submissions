e7f9004ae0e1c9435504dac2f6902bd92484e7fa
Danyal Abbas <abasdaniel61@gmail.com>
2024-10-10 20:51:22 +0500

    issues

---
+++ PF Labs/Lab #6/prob4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char username[6];
+    printf("hello : ");
+    scanf("%s", &username);
+
+    printf("%s", username);
+}
---
+++ PF Labs/Lab #6/prob5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int i;
+    double j = 54.5435464565475675675757;
+    for (i=2;i<10;i++)
+        printf("%lf",j);
+}
