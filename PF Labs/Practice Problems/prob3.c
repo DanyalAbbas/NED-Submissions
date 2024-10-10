a5bd00f15dd518969a97d86fbb99764c7b3d406a
Danyal Abbas <abasdaniel61@gmail.com>
2024-10-10 17:19:41 +0500

    hm

--- PF Labs/Practice Problems/prob3.c
+++ PF Labs/Practice Problems/prob3.c
@@ -6,7 +6,7 @@ int main()
     printf("Give man the year innit?!? : ");
     scanf("%d", &year);
 
-    if (year % 4 == 0 && year % 400 == 0)
+    if (year % 4 == 0 && (year % 400 == 0 || year % 100 != 0))
         printf("BOMBACLAAT THE YEAR BE LEAPING YKNOW!?");
     else
         printf("SHUTUP FAM, THIS AINT NO LEAP YEAR YOU JOKEMAN");
