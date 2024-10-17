b8824c4b4be416c5dfb4911f96217930605c965e
Danyal Abbas <abasdaniel61@gmail.com>
2024-10-17 16:21:43 +0500

    [Created] Inverted Left Half

---
+++ PF Labs/Pattern Practice/Inverted_Left_Half.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int rows;
+    rows = 5;
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            printf("  ");
+        }
+        for (int j = 0; j < rows - i; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
