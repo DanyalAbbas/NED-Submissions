85736c7ad1e8fe92cbed75c94f15f00aa97f6ebf
Danyal Abbas <abasdaniel61@gmail.com>
2024-10-29 10:40:48 +0500

    add lab 9 problem 1

---
+++ PF Labs/Lab #9/prob1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void CountFrequency(int size);
+int main()
+{
+    int arr_size = 5;
+    CountFrequency(arr_size);
+
+
+}
+
+void CountFrequency(int size)
+{
+    int freq[11] = {0};
+    int arr[] = {2,3,5,6,0};
+
+    for (int i = 0; i < size; i++)
+    {
+        freq[arr[i]]++;
+    }
+
+    for (int i = 0; i <= size; i++)
+        printf("%d : %d\n", i, freq[i]);
+}
