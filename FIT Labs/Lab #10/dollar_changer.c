0c126210f0515b7ad61d3dc0014cadd2eb9fced6
Danyal Abbas <abasdaniel61@gmail.com>
2024-11-07 15:22:06 +0500

    branch :0

--- FIT Labs/Lab #10/dollar_changer.c
+++ FIT Labs/Lab #10/dollar_changer.c
@@ -2,13 +2,15 @@
 
 void main()
 {
-    float dollar, pkr;
+    float dollar, pkr, euro;
     printf("Enter paisa in dallar : ");
     scanf("%f", &dollar);
 
     pkr = 280 * dollar;
+    euro = 0.9 * dollar;
 
-    printf("\nThe converted money is %.2f :0", pkr);
+    printf("\nThe converted money from rupee is %.2f :0", pkr);
+    printf("\nThe converted money from euro is %.2f :0", euro);
     
 
 }
