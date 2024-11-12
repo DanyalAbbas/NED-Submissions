ac9b4dbe4871382ed6e9a51cec695e503b66df3c
Danyal Abbas <abasdaniel61@gmail.com>
2024-11-12 12:41:58 +0500

    Add files via upload

--- PF Labs/Lab #11/prob4.c
+++ PF Labs/Lab #11/prob4.c
@@ -3,8 +3,9 @@
 int main()
 {
     int len;
-    float arr[] = {1.5,3.5,6.4,10.0,2.0};
-    float highest = 0, sec_highest = arr[0];
+    float arr[] = {3.14, 2.71, 9.81, 1.61, 4.67, 5.99, 0.57, 8.23, 7.12, 6.45, 
+                   0.99, 3.33, 4.45, 6.28, 5.55, 7.89, 2.22, 8.91, 1.11, 0.76};
+    float highest = arr[0], sec_highest = 0;
     float *ptr = arr;
 
     len = sizeof(arr)/sizeof(arr[0]);
@@ -16,10 +17,11 @@ int main()
             sec_highest = highest;
             highest = *(ptr + i);
         }
-        
+        if (*(ptr + i) > sec_highest && *(ptr + i) < highest)
+            sec_highest = *(ptr + i);
     }
 
-    printf("%f", sec_highest);
+    printf("%.2f", sec_highest);
 
 
 }
