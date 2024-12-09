1ae0f67a517f74b344e7501ee10fe2dff5246817
Danyal Abbas <abasdaniel61@gmail.com>
2024-12-09 18:56:40 +0500

    completed the palindrome exercise thingey

--- PF Labs/Lab #6/prob3.c
+++ PF Labs/Lab #6/prob3.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
 	int number, i, add = 0, add2 = 0;
 	printf("Input any 5 digit number please >:3 : ");
 	scanf("%d", &number);
+
+	char pali[5]; 
 	
+	i = 0;
 	while (number >= 1)
 	{
 		add += number%10;
+		pali[i] = number%10;
+		i++;
 		number /= 10;
 		printf("%d ", add);
 		printf("%d\n", number);
@@ -25,8 +31,24 @@ int main()
 				break;	
 			}	
 		}
-		if (number != 0)
+		if (add != 0)
 			printf("\nThe number is indeed prime >:3");	
+	}
+	else
+	{
+		for (int j = 0, k = 4; j<5; j++,k--)
+		{
+			if (pali[j] != pali[k])
+			{
+				i = 69;
+				break;
+			}
+		}
+		if (i == 69)
+			printf("The number is not a palindrome");
+		else
+			printf("The number is indeed palindrome");
+		
 	}
 	
 }
