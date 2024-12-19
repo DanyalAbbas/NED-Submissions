55418216201b828008e25d6359ed25f3bf220d46
Danyal Abbas <abasdaniel61@gmail.com>
2024-12-19 15:09:54 +0500

    i kms

--- PF Labs/Finals Practice/test.c
+++ PF Labs/Finals Practice/test.c
@@ -2,12 +2,23 @@
 
 int main()
 {
-    int a = 8, b = 13, x = 0, y = 10;
-    for (int k = --a; k < b; k++)
+    // int a = 8, b = 13, x = 0, y = 10;
+    // for (int k = --a; k < b; k++)
+    // {
+    //     x = x + k;
+    //     printf("%d\n", y);
+    //     y = y - x--;
+    //     printf("%d %d\n", x,y);
+    // }
+    int i = 0, j = 0;
+    for (i = 0; i < 5; i++)
     {
-        x = x + k;
-        printf("%d\n", y);
-        y = y - x--;
-        printf("%d %d\n", x,y);
+        for (j = 0; j < 3;j++)
+        {;}
+
     }
+
+    printf("%d %d", i, j);
+
+
 }
