2dc57e4fcbcb6c8dd10bc9ad06841d21a6d20499
Danyal Abbas <abasdaniel61@gmail.com>
2025-09-07 23:22:29 +0500

    Dsa lab 1 prob 3

---
+++ 3rd sem (Ongoing)/DSA/Lab #1/prob3.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+class CalculateMedian{
+	private:
+		int arr[50000];
+		int size;
+	public:
+		CalculateMedian() : size(0) {}
+		
+		void addnum(int num)
+        {
+			int i=size-1;
+			while(i>=0 && arr[i]>num)
+            {
+				arr[i+1]=arr[i];
+				i--;
+			}
+			arr[i+1]=num;
+			size++;
+		}
+		
+		double findmedian()
+        {
+			if(size%2==1) {return arr[(size/2)];}
+			else
+            {
+				int mid=size/2;
+				return(arr[mid-1]+arr[mid])/(double)2;
+			}
+		}
+};
+
+int main(void)
+{
+	CalculateMedian mf;
+	cout<<"Enter numbers to find median (-1 to stop): "<<endl;
+    int num;
+    do
+    {
+    	cin>>num;
+    	mf.addnum(num);
+    } while (num!=-1);
+
+	cout<<"median: " << mf.findmedian()<<endl;
+	return 0;
+}
