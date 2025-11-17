4e4a6ad9682f37656c37dc1370d746afcab89d4d
Danyal Abbas <abasdaniel61@gmail.com>
2025-11-17 10:57:37 +0500

    Update prob1.cpp

--- 3rd sem (Ongoing)/DSA/Lab #10/prob1.cpp
+++ 3rd sem (Ongoing)/DSA/Lab #10/prob1.cpp
@@ -4,3 +4,28 @@ using namespace std;
 /*Complete implementation of AVL tree given in Example 01 by implementing Insert and Delete
 methods.*/
 
+class Node {
+public:
+    int key;
+    Node* left;
+    Node* right;
+    int height;
+
+    Node(int val) {
+        key = val;
+        left = NULL;
+        right = NULL;
+        height = 1;
+    }
+};
+
+int height(Node* N) {
+    if (N == NULL)
+        return 0;
+    return N->height;
+}
+int getBalance(Node* N) {
+    if (N == NULL)
+        return 0;
+    return height(N->left) - height(N->right);
+}
